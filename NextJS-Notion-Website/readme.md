b3eb4ae6bbd55f886914ad5f8abcae5f67a3e475
Clark Weckmann <accounts@weckmann.me>
2023-05-11 10:22:11 -0500

    radme

--- NextJS-Notion-Website/readme.md
+++ NextJS-Notion-Website/readme.md
@@ -1,2 +1,18 @@
 # NextJS Plain Notion Websites
+
 See `site.config.ts` for configuration options.
+
+# Deploying to Vercel
+
+- Visit [vercel.com](https://vercel.com)
+- Create a free account with github
+- Link github repos
+- Click `New` button to deploy from github and select you Notion website repo.
+
+# CloudFlare
+
+- Create a new cloudflare service worker
+- Select `Quick Edit`
+- Copy the worker.js file
+- Paste it
+- Select `Save & Deploy`
--- NextJS-Notion-Website/site.config.ts
+++ NextJS-Notion-Website/site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3a09bf1d8bff42f0a9c8d3c6a92ea62f',
+  rootNotionPageId: '3a09bf1d8bff42f0a9c8d3c6a92ea62f', // Notion ID
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Next.js Notion Starter Kit', // Title of your site
   domain: 'nextjs-notion-starter-kit.vercel.app',
   author: 'Clark Weckmann',
 
